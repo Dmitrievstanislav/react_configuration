8462527365e4b01572c1082ad0c15eb23483bed1
Massacre <massacre@Massacres-MacBook-Air.local>
2022-10-09 23:20:05 +0600

    Added sidebar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,9 @@
-import { Link } from "react-router-dom";
 import "./styles/index.scss";
 import { classNames } from "shared/lib/classNames/classNames";
 import { useTheme } from "app/providers/ThemeProviders";
 import { AppRouter } from "./providers/router";
 import { Header } from "widgets/Header";
+import { Sidebar } from "widgets/Sidebar";
 
 
 
@@ -11,11 +11,12 @@ const App = () => {
     const { theme } = useTheme();
 
     return (
-        <div>
-            <div className={classNames("app", {}, [theme])}>
-                <Header />
+        <div className={classNames("app", {}, [theme])}>
+            <Header />
+            <div className="content-page">
+                <Sidebar />
+                <AppRouter />
             </div>
-            <AppRouter />
         </div>
     );
 };
--- src/widgets/Header/ui/Header.tsx
+++ src/widgets/Header/ui/Header.tsx
@@ -1,7 +1,5 @@
-import { Link } from "react-router-dom";
 import { classNames } from "shared/lib/classNames/classNames";
 import { AppLink, AppLinkTheme } from "shared/ui/AppLink/AppLink";
-import { ThemeSwitcher } from "shared/ui/ThemeSwitcher";
 import cls from "./Header.module.scss"
 
 
@@ -12,7 +10,6 @@ interface HeaderProps {
 export const Header = ({className}: HeaderProps) => {
     return (
         <header className={classNames(cls.Header, {}, [className])}>
-            <ThemeSwitcher />
             <nav className={cls.links}>
                 <AppLink theme={AppLinkTheme.SECONDARY} className={cls.mainLink}to={"/"}>Главная</AppLink>
                 <AppLink theme={AppLinkTheme.SECONDARY} to={"/about"}>О нас</AppLink>

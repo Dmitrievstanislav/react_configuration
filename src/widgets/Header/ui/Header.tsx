a8a6155188eed093add8970fe105ba18d19691ae
Massacre <massacre@Massacres-MacBook-Air.local>
2022-10-20 18:12:48 +0600

    Added jest ans test for classNames

--- src/widgets/Header/ui/Header.tsx
+++ src/widgets/Header/ui/Header.tsx
@@ -1,3 +1,4 @@
+import {useTranslation} from 'react-i18next';
 import {classNames} from 'shared/lib/classNames/classNames';
 import {AppLink, AppLinkTheme} from 'shared/ui/AppLink/AppLink';
 import cls from './Header.module.scss';
@@ -8,15 +9,16 @@ interface HeaderProps {
 }
 
 export const Header = ({className}: HeaderProps) => {
+  const {t} = useTranslation();
   return (
     <header className={classNames(cls.header, {}, [className])}>
       <nav className={cls.links}>
         <AppLink
           theme={AppLinkTheme.SECONDARY}
-          className={cls.mainLink}to={'/'}>Главная</AppLink>
+          className={cls.mainLink}to={'/'}>{t('Главная')}</AppLink>
         <AppLink
           theme={AppLinkTheme.SECONDARY}
-          to={'/about'}>О нас</AppLink>
+          to={'/about'}>{t('О нас')}</AppLink>
       </nav>
     </header>
   );
--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -1,4 +1,5 @@
 import {useState} from 'react';
+import {useTranslation} from 'react-i18next';
 import {classNames} from 'shared/lib/classNames/classNames';
 import {LangSwitcher} from 'shared/ui/LangSwitcher/LangSwitcher';
 import {ThemeSwitcher} from 'shared/ui/ThemeSwitcher/ThemeSwitcher';
@@ -10,6 +11,7 @@ interface SidebarProps {
 }
 
 export const Sidebar = ({className}: SidebarProps) => {
+  const {t} = useTranslation();
   const [collapsed, setCollapsed] = useState(false);
   const onToogle = () => {
     setCollapsed((prev) => !prev);
@@ -23,7 +25,7 @@ export const Sidebar = ({className}: SidebarProps) => {
           [className],
       )}
     >
-      <button onClick={onToogle}>toogle</button>
+      <button onClick={onToogle}>{t('Переключить')}</button>
       <div className={cls.switchers}>
         <ThemeSwitcher />
         <LangSwitcher className={cls.lang}/>

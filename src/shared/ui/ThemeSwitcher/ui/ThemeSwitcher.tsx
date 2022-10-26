c9bb73355902e46c805fcc33b7c3cae3bb6968ca
Massacre <massacre@Massacres-MacBook-Air.local>
2022-10-26 17:41:19 +0600

    Placed svg to center ThemeSwitcher

--- src/shared/ui/ThemeSwitcher/ui/ThemeSwitcher.tsx
+++ src/shared/ui/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -3,6 +3,7 @@ import {classNames} from 'shared/lib/classNames/classNames';
 import LightIcon from 'shared/assets/icons/theme_light.svg';
 import DarkIcon from 'shared/assets/icons/theme_dark.svg';
 import {Button, ThemeButton} from 'shared/ui/Button';
+import cls from './ThemeSwitcher.module.scss';
 
 
 interface ThemeSwitcherProps {
@@ -15,7 +16,7 @@ export const ThemeSwitcher = ({className}: ThemeSwitcherProps) => {
   return (
     <Button
       theme={ThemeButton.CLEAR}
-      className={classNames('', {}, [className])}
+      className={classNames(cls.btn, {}, [className])}
       onClick={toogleTheme}
     >
       {theme === Theme.DARK ?

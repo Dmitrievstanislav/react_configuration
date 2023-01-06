3997624430f2b2c3cff8ce20f75b9252c4f944d5
dmitrievstanislav <dmitrievartstation@gmail.com>
2023-01-06 18:25:19 +0600

    Styled sidebar

--- src/shared/ui/ThemeSwitcher/ui/ThemeSwitcher.tsx
+++ src/shared/ui/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -4,10 +4,12 @@ import LightIcon from 'shared/assets/icons/theme_light.svg';
 import DarkIcon from 'shared/assets/icons/theme_dark.svg';
 import {Button, ButtonTheme} from 'shared/ui/Button';
 import cls from './ThemeSwitcher.module.scss';
+import {t} from 'i18next';
+import {clearDecorators} from '@storybook/react/dist/ts3.9/client/preview';
 
 
 interface ThemeSwitcherProps {
-    className?: string,
+    className?: string
 }
 
 export const ThemeSwitcher = ({className}: ThemeSwitcherProps) => {
@@ -20,8 +22,8 @@ export const ThemeSwitcher = ({className}: ThemeSwitcherProps) => {
       onClick={toogleTheme}
     >
       {theme === Theme.DARK ?
-            <DarkIcon /> :
-            <LightIcon />
+              <DarkIcon className={cls.icon}/>:
+              <LightIcon className={cls.icon}/>
       }
     </Button>
   );
--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -1,10 +1,15 @@
 import {useState} from 'react';
 import {useTranslation} from 'react-i18next';
 import {classNames} from 'shared/lib/classNames/classNames';
-import { Button, ButtonTheme } from 'shared/ui/Button';
+import {AppLink, AppLinkTheme} from 'shared/ui/AppLink/AppLink';
+import {Button, ButtonTheme} from 'shared/ui/Button';
 import {LangSwitcher} from 'shared/ui/LangSwitcher';
 import {ThemeSwitcher} from 'shared/ui/ThemeSwitcher';
 import cls from './Sidebar.module.scss';
+import HomeIcon from 'shared/assets/icons/homeIcon.svg';
+import ListIcon from 'shared/assets/icons/listIcon.svg';
+import ArrowLeftIcon from 'shared/assets/icons/arrowLeftIcon.svg';
+import ArrowRightIcon from 'shared/assets/icons/arrowRightIcon.svg';
 
 
 interface SidebarProps {
@@ -26,17 +31,45 @@ export const Sidebar = ({className}: SidebarProps) => {
           [className],
       )}
     >
-      <Button
-        data-testid='sidebar-toogle'
-        onClick={onToogle}
-        className={cls.collapseBtn}
-        theme={ButtonTheme.BACKGROUND}
-        >
-        {collapsed ? '>' : '<'}
-      </Button>
+      <nav>
+        <ul>
+          <li>
+            <AppLink
+              className={cls.item}
+              theme={AppLinkTheme.SECONDARY}
+              to={'/'}
+            >
+              <HomeIcon className={cls.icon}/>
+              <span className={cls.link}>
+                {t('Главная')}
+              </span>
+            </AppLink>
+          </li>
+          <li>
+            <AppLink
+              className={cls.item}
+              theme={AppLinkTheme.SECONDARY}
+              to={'/about'}
+            >
+              <ListIcon className={cls.icon}/>
+              <span className={cls.link}>
+                {t('О нас')}
+              </span>
+            </AppLink>
+          </li>
+        </ul>
+      </nav>
       <div className={cls.switchers}>
-        <ThemeSwitcher />
-        <LangSwitcher short={collapsed}/>
+        <ThemeSwitcher className={cls.switcher}/>
+        <LangSwitcher className={cls.switcher} short={collapsed}/>
+        <Button
+          data-testid='sidebar-toogle'
+          onClick={onToogle}
+          className={cls.switcher}
+          theme={ButtonTheme.BACKGROUND}
+        >
+          {collapsed ? <ArrowRightIcon /> : <ArrowLeftIcon />}
+        </Button>
       </div>
     </div>
   );

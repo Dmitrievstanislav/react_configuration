78b3e73bbecbfcee215601f4465fb8fd1bd0d5a3
dmitrievstanislav <dmitrievartstation@gmail.com>
2023-03-06 21:23:19 +0600

    Edited syntax error

--- json-server/index.js
+++ json-server/index.js
@@ -27,7 +27,7 @@ server.post('/login', (req, res) => {
     const {users = []} = db;
 
     const userFromBd = users.find(
-        (user) => user.username === username && user.password === password
+        (user) => user.username === username && user.password === password,
     );
 
     if (userFromBd) {

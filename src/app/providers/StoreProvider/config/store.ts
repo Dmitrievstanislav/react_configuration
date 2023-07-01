27ae827cd99db96a096964bbaa48d17ddde77370
Dmitrsta <dmitrievartstation@gmail.com>
2023-07-01 15:42:43 +0400

    fix screenshot loki

--- src/app/providers/StoreProvider/config/store.ts
+++ src/app/providers/StoreProvider/config/store.ts
@@ -7,7 +7,7 @@ export function createReduxStore(initialState?: StateSchema) {
   const rootReducers: ReducersMapObject<StateSchema> = {
     counter: counterReducer,
     user: userReducer,
-  }
+  };
   return configureStore<StateSchema>({
     reducer: rootReducers,
     devTools: __IS_DEV__,
